fd4157e893f1a53b45be455c3e3b07311f62a9bd
anurago9o9 <58781750+anurago9o9@users.noreply.github.com>
2024-02-01 00:56:21 +0530

    Updates

---
+++ precache-manifest.bb8b67654053fe598af84508fceef9d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b823d98bffa7c1eb6afcbadd1bd3683",
+    "url": "/Kyupid_dashboard/index.html"
+  },
+  {
+    "revision": "0edd51309963cb3f4eb7",
+    "url": "/Kyupid_dashboard/static/css/2.3628c8c4.chunk.css"
+  },
+  {
+    "revision": "dfff34334d48f3a0d049",
+    "url": "/Kyupid_dashboard/static/css/main.3537fa76.chunk.css"
+  },
+  {
+    "revision": "0edd51309963cb3f4eb7",
+    "url": "/Kyupid_dashboard/static/js/2.fa6334a1.chunk.js"
+  },
+  {
+    "revision": "3565909536a3b3e7784b801f86b2d3a9",
+    "url": "/Kyupid_dashboard/static/js/2.fa6334a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfff34334d48f3a0d049",
+    "url": "/Kyupid_dashboard/static/js/main.ae49e2fb.chunk.js"
+  },
+  {
+    "revision": "4f371207ac8c8da1116f75fbcf1f124b",
+    "url": "/Kyupid_dashboard/static/js/mapbox-gl-csp-worker.f0217a07.worker.js"
+  },
+  {
+    "revision": "d87e1036281b747d0263",
+    "url": "/Kyupid_dashboard/static/js/runtime-main.eb7f50ac.js"
+  }
+]);
